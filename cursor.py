3093f7959de9a6006c49860901b0c6f6dbaea3c8
TheeEmuu <chertangoldsmith@gmail.com>
2020-12-27 06:04:00 -0500

    implemented bot

---
+++ cursor.py
@@ -0,0 +1,8 @@
+import pyautogui as g
+import time
+
+while(True):
+    print(g.position())
+    time.sleep(2)
+
+
---
+++ read.py
@@ -0,0 +1,126 @@
+from PIL import Image, ImageEnhance, ImageFilter
+from PIL import ImageGrab
+import cv2
+import pyautogui as mouse
+
+import json
+
+import time
+
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'D:\\Programs\\Tesseract\\tesseract.exe'
+
+# # read from main screen
+# for i in range(0, 3):
+#     for i in range(0, 10):
+#         w = ImageGrab.grab(bbox=(1100,65 + 80 * i,1250,85 + 80 * i))
+#         w = pytesseract.image_to_string(w).strip().replace("\n\n"," ")
+#     mouse.click(1450,900)
+#     time.sleep(1)
+
+def scrollDown():
+    for i in range(0, 5):
+        mouse.scroll(-5000)
+    time.sleep(0.5)
+
+def scrollUp():
+    for i in range(0, 5):
+        mouse.scroll(5000)
+    time.sleep(0.5)
+
+def getName():
+    w = ImageGrab.grab(bbox=(1265,150,1500,175))
+    name = pytesseract.image_to_string(w).strip().replace("\n\n"," ")
+    return name
+
+def click(x, y):
+    mouse.click(x, y)
+    mouse.moveTo(1400,65)
+    time.sleep(0.5)
+
+toJson = []
+
+click(1250, 100)
+
+for k in range(0,3):
+    current = {
+        "name": "missingno",
+        "partners" : [],
+        "moves" : [],
+        "ability" : [],
+        "nature" : [],
+        "item" : []
+    }
+
+    #name
+    name = getName()
+    current["name"] = name
+
+    #partners
+    scrollDown()
+    if(name != getName()):
+        for i in range(0,10):
+            w = ImageGrab.grab(bbox=(1100, 110 + 80 * i, 1250, 135 + 80 * i))
+            w = pytesseract.image_to_string(w).strip().replace("\n\n"," ")
+            current["partners"].append(w)
+    else:
+        for i in range(0,6):
+            w = ImageGrab.grab(bbox=(1100, 435 + 80 * i, 1250, 510 + 80 * i))
+            w = pytesseract.image_to_string(w).strip().replace("\n\n"," ")
+            current["partners"].append(w)
+    scrollUp()
+
+
+    #moves
+    click(1135, 360)
+    scrollDown()
+    if(name != getName()):
+        for i in range(0,10):
+            w = ImageGrab.grab(bbox=(1100, 185 + 65 * i, 1550, 220 + 65 * i))
+            w = pytesseract.image_to_string(w).strip().replace("\n\n"," ")
+            current["moves"].append(w)
+    else:
+        for i in range(0,8):
+            w = ImageGrab.grab(bbox=(1100, 430 + 65 * i, 1550, 465 + 65 * i))
+            w = pytesseract.image_to_string(w).strip().replace("\n\n"," ")
+            current["moves"].append(w)
+    scrollUp()
+
+    #abilites
+    click(1230,360)
+    for i in range(0,5):
+        w = ImageGrab.grab(bbox=(1100, 435 + 65 * i, 1550, 465 + 65 * i))
+        w = pytesseract.image_to_string(w).strip().replace("\n\n"," ")
+        current["ability"].append(w)
+
+    #nature
+    click(1325, 360)
+    for i in range(0,5):
+        w = ImageGrab.grab(bbox=(1100, 435 + 65 * i, 1550, 465 + 65 * i))
+        w = pytesseract.image_to_string(w).strip().replace("\n\n"," ")
+        current["nature"].append(w)
+
+    #item
+    click(1425,360)
+    for i in range(0,5):
+        w = ImageGrab.grab(bbox=(1100, 435 + 65 * i, 1550, 465 + 65 * i))
+        w = pytesseract.image_to_string(w).strip().replace("\n\n"," ")
+        current["item"].append(w)
+
+    toJson.append(current)
+    # with open("data.json") as json_file:
+    #     feeds = json.load(json_file)
+
+    # feeds.append(current)
+
+    # with open("data.json", 'w') as json_file:
+    #     json.dump(feeds, json_file, indent = 2)
+    
+
+    time.sleep(2)
+    click(1552,186)
+
+
+with open("data.json", 'w') as json_file:
+    json.dump(toJson, json_file, indent=2)
